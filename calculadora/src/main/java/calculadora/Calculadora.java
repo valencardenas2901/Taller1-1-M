e229d54ff4d4ea51ff88e082aadc42e09c188b91
valencardenas2901 <141367285+valencardenas2901@users.noreply.github.com>
2023-08-10 15:06:06 -0500

    Add files via upload

--- calculadora/src/main/java/calculadora/Calculadora.java
+++ calculadora/src/main/java/calculadora/Calculadora.java
@@ -6,13 +6,19 @@ public class Calculadora {
   public static void main(String[] args) {
     Scanner leer = new Scanner(System.in);
     int opcion;
-    int num1 = 0;
-    int num2 = 0;
-    int respuesta;
+    float num1 = 0;
+    float num2 = 0;
+    float respuesta;
 
     do {
-      System.out.println("Elige la operacion a realizar:\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Salir");
+      System.out.println("Elige la operacion a realizar:\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Seno\n6. Coseno\n7. Tangente\n8. Salir");
       opcion = leer.nextInt();
+      
+      System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+      System.out.println("Digite el segundo numero:");
+      num2 = leer.nextInt();
 
       switch (opcion) {
       case 1 -> {
@@ -40,19 +46,17 @@ public class Calculadora {
         break;
       }
       case 5 -> {
+        respuesta = (float)Math.sin(num1);
+        System.out.println("El seno del primer numero es: " + respuesta);
+        break;
+      }
+      case 8 -> {
         continue;
       }
       }
       
       
       
-      System.out.println("Digite el primer numero:");
-      do {
-        num1 = leer.nextInt();
-      } while (num1 < 0 || num1 > 200);
-
-      System.out.println("Digite el segundo numero:");
-      num2 = leer.nextInt();
 
     } while (opcion != 5);
 
