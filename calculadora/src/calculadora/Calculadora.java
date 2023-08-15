f5475995c4b2ee4e364c054bbe2a88a1ebce8333
valencardenas2901 <141367285+valencardenas2901@users.noreply.github.com>
2023-08-14 22:11:06 -0500

    Add files via upload

--- calculadora/src/calculadora/Calculadora.java
+++ calculadora/src/calculadora/Calculadora.java
@@ -13,30 +13,50 @@ public class Calculadora {
     do {
       System.out.println("Elige la operacion a realizar:\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Seno\n6. Coseno\n7. Tangente\n8. Salir");
       opcion = leer.nextInt();
-      
-      System.out.println("Digite el primer numero:");
-        num1 = leer.nextInt();
-
-      System.out.println("Digite el segundo numero:");
-      num2 = leer.nextInt();
 
       switch (opcion) {
       case 1 -> {
+        System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+        System.out.println("Digite el segundo numero:");
+        num2 = leer.nextInt();
+
         respuesta = num1 + num2;
         System.out.println("La suma de ambos numeros es: " + respuesta);
         break;
       }
       case 2 -> {
+        System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+        System.out.println("Digite el segundo numero:");
+        num2 = leer.nextInt();
+
         respuesta = num1 - num2;
         System.out.println("La resta de ambos numeros es: " + respuesta);
         break;
       }
       case 3 -> {
+
+        System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+        System.out.println("Digite el segundo numero:");
+        num2 = leer.nextInt();
+
         respuesta = num1 * num2;
         System.out.println("El producto de ambos numeros es: " + respuesta);
         break;
       }
       case 4 -> {
+
+        System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+        System.out.println("Digite el segundo numero:");
+        num2 = leer.nextInt();
+
         if (num2 == 0) {
           System.out.println("Esta respuesta es indefinida");
         } else {
@@ -46,30 +66,52 @@ public class Calculadora {
         break;
       }
       case 5 -> {
-        respuesta = (float)Math.sin(num1);
+
+        System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+        System.out.println("Digite el segundo numero:");
+        num2 = leer.nextInt();
+
+        respuesta = (float) Math.sin(num1);
         System.out.println("El seno del primer numero en radianes es: " + respuesta);
+        respuesta = (float) Math.sin(num2);
+        System.out.println("El seno del segundo numero en radianes es: " + respuesta);
         break;
       }
       case 6 -> {
-        respuesta = (float)Math.cos(num1);
+
+        System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+        System.out.println("Digite el segundo numero:");
+        num2 = leer.nextInt();
+
+        respuesta = (float) Math.cos(num1);
         System.out.println("El coseno del primer numero es: " + respuesta);
+        respuesta = (float) Math.cos(num2);
+        System.out.println("El coseno del segundo numero es: " + respuesta);
         break;
       }
       case 7 -> {
-        respuesta = (float)Math.tan(num1);
+
+        System.out.println("Digite el primer numero:");
+        num1 = leer.nextInt();
+
+        System.out.println("Digite el segundo numero:");
+        num2 = leer.nextInt();
+
+        respuesta = (float) Math.tan(num1);
         System.out.println("La tangente del primer numero es: " + respuesta);
+        respuesta = (float) Math.tan(num2);
+        System.out.println("La tangente del segundo numero es: " + respuesta);
         break;
       }
       case 8 -> {
-        continue;
+        break;
       }
       }
-      
-      
-      
-
     } while (opcion != 8);
-
 
   }
 }
